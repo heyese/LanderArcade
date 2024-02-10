061e1cc71d8958757cbf0968dbb3e067b3c4682e
Edmund Heyes <e_heyes@hotmail.com>
2024-02-10 19:28:39 +0000

    Missiles chooses shortest path to Lander

--- classes/missile.py
+++ classes/missile.py
@@ -1,8 +1,9 @@
 import arcade
-from constants import SCALING
+from constants import SCALING, WORLD_WIDTH
 from classes.world import World
 from classes.game_object import GameObject
 from classes.engine import Engine
+from classes.lander import Lander
 
 
 class Missile(GameObject):
@@ -21,7 +22,15 @@ class Missile(GameObject):
     def on_update(self, delta_time: float = 1 / 60):
         super().on_update(delta_time=delta_time)
         if self.engine.activated:
+            # I want the missile to take the shortest route to the lander - and that might
+            # involve a world wrap.
+            points = [self]
             if lander_sprite_list := self.scene.name_mapping.get("Lander"):
-                self.face_point(lander_sprite_list[0].position)
+                lander: Lander = lander_sprite_list[0]
+                points = [lander.position,
+                          (lander.position[0] - (WORLD_WIDTH - 2 * self.world.camera_width), lander.position[1]),
+                          (lander.position[0] + (WORLD_WIDTH - 2 * self.world.camera_width), lander.position[1])]
+                point = sorted(points, key=lambda p: arcade.get_distance(*self.position, *p))[0]
+                self.face_point(point)
         else:
             self.engine.activate()
