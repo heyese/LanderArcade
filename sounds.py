28867f6d7d2e393a1a08e6ce09f01e354b751b9a
Edmund Heyes <e_heyes@hotmail.com>
2024-03-07 12:42:09 +0000

    Sounds pan and volume done

--- sounds.py
+++ sounds.py
@@ -12,11 +12,25 @@ if TYPE_CHECKING:
 GAME_OBJECTS = constants.GAME_OBJECTS
 
 
+def get_lander_and_camera():
+    lander: Lander = constants.GAME_OBJECTS["lander"]
+    camera: arcade.Camera = constants.GAME_OBJECTS["camera"]
+    return lander, camera
 
-def get_pan(sound_position):
+
+def get_pan(position):
     """Pan (-1 is fully left, 0 is centre, 1 is fully right) of sound as
     determined by relative positions of sound and lander"""
-    return 0
+    lander, camera = get_lander_and_camera()
+    if not lander and camera:
+        return 0
+    # This just depends on position.X, not the Y values
+    # I suddenly kind of wonder if, when an object is on the left, we should pan fully on the left.  And the same
+    # for the right.  In the 2D sense, that feels appropriate - but I don't think it's what I want.
+    # I think I want it to be fully on the left, and then gradually change to zero when they have the same x coordinates,
+    # and then pan to the right.
+    pan = min(max(-1, (position[0] - lander.center_x) / camera.viewport_width), 1)
+    return pan
 
 
 def get_volume_multiplier(position: tuple[float, float]) -> float:
@@ -25,10 +39,10 @@ def get_volume_multiplier(position: tuple[float, float]) -> float:
     # the volume of individual sounds.  This functions should just turn that up and down depending on where the lander is
     # I think the idea will be that you can't hear objects greater than a screen's width from the lander, and the
     # volume increases as it gets closer
-    lander: Lander = constants.GAME_OBJECTS["lander"]
-    camera: arcade.Camera = constants.GAME_OBJECTS["camera"]
-    if not lander:
+    lander, camera = get_lander_and_camera()
+    if not lander and camera:
         return 1
+    # Don't need to worry about world wrapping here, because the game ensures sprites are always on the side of the lander
     distance = collisions.modulus((lander.center_x - position[0], lander.center_y - position[1]))
     if distance > camera.viewport_width:
         return 0
